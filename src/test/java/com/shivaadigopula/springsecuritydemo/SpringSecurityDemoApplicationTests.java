6d07d15714150e521b1ef50e151d392a3739ed5f
Shiva Adigopula <vskrao.adigopula@gmail.com>
2023-01-12 10:25:50 +0400

    first commit

---
+++ src/test/java/com/shivaadigopula/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivaadigopula.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
